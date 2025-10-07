2db16a4fa37f62a3698bf77fcc01135743f73226
unknown <dgoodwin3@una.edu>
2025-10-07 04:46:05 -0500

    fixed server issue?

--- app.mjs
+++ app.mjs
@@ -20,7 +20,13 @@ app.use(express.static(PUBLIC_PATH)); // Serve static files (auth.html, cve.html
 
 // --- MONGODB CONNECTION ---
 const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri);
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 let db; // Global variable to hold the connected database instance
 
 async function connectToMongo() {
