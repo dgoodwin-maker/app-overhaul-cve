a0ae7e0c6d936c47c9da7444e70a0c9d283a4596
unknown <dgoodwin3@una.edu>
2025-10-07 20:19:36 -0500

    Fixed Sample Data v4?

--- app.mjs
+++ app.mjs
@@ -13,7 +13,7 @@ const port = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 // Correctly map the public folder path
-const PUBLIC_PATH = path.join(__dirname, 'public');
+const PUBLIC_PATH = path.join(__dirname, 'src', 'public');
 
 // Middleware
 app.use(express.urlencoded({ extended: true })); // To parse form data (for /register)
